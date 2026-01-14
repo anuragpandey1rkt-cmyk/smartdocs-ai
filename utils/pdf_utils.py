0f58d637be7126e1abbfeef206e71389ac73f7b7
ANURAG PANDEY <anuragpandey1rkt@gmail.com>
2026-01-14 22:52:39 +0530

    Delete utils directory

--- utils/pdf_utils.py
+++
@@ -1,9 +0,0 @@
-from PyPDF2 import PdfReader
-
-def extract_text(pdf):
-    reader = PdfReader(pdf)
-    text = ""
-    for page in reader.pages:
-        if page.extract_text():
-            text += page.extract_text()
-    return text
