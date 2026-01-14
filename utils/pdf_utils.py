bf817228e74eea17ad22ae833b0617133a6d4767
ANURAG PANDEY <anuragpandey1rkt@gmail.com>
2026-01-14 19:44:11 +0530

    Create pdf_utils.py

---
+++ utils/pdf_utils.py
@@ -0,0 +1,9 @@
+from PyPDF2 import PdfReader
+
+def extract_text(pdf):
+    reader = PdfReader(pdf)
+    text = ""
+    for page in reader.pages:
+        if page.extract_text():
+            text += page.extract_text()
+    return text
